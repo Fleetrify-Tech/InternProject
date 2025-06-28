6ca7f3079c760f6067a0056c584633760b807eea
Kehinde211 <97588309+Kehinde211@users.noreply.github.com>
2025-06-28 21:19:37 +0100

    Update UserTable.tsx

--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -10,7 +10,7 @@ interface Props {
 export default function UserTable({ users, onEdit, onDelete }: Props) {
   return (
     <div className="flex flex-col sm:flex-row">
-      <table className="min-w-full  bg-white">
+      <table className="min-w-full  bg-white"> {// Table Headers // }
         <thead>
           <tr>
             <th className=" px-4 py-2">First Name</th>
@@ -20,12 +20,14 @@ export default function UserTable({ users, onEdit, onDelete }: Props) {
           </tr>
         </thead>
         <tbody>
+        {// Table data gets rendered upon completion // }
           {users.map((user) => (
             <tr key={user.id}>
               <td className=" px-2">{user.firstName}</td>
               <td className=" px-2">{user.lastName}</td>
               <td className=" px-2">{user.dob}</td>
               <td className="flex gap-1 py-2 px-2">
+                {// Buttons for editing and deleting the user form data//}
                 <button
                   className=" p-1 rounded-md hover:bg-blue-600 hover:text-white"
                   onClick={() => onEdit(user)}
